df414c56119b2f2577761c98118c11e010c0d773
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-11-06 09:09:40 +0600

    with build

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className={banglaFont.className}>
       <section className="flex min-h-[calc(100vh-64px)] bg-primary w-full flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center bg-white px-20 py-5 rounded-sm shadow-inner shadow-black/50 -rotate-3 hover:rotate-0">
+        <div className="flex flex-col items-center bg-white px-20 py-5 rounded-sm shadow-inner shadow-black/60 -rotate-3 hover:rotate-0">
           <Image
             src={logo}
             height={100}
